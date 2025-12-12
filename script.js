2231246bf6440ce3e5a93c9649f94dba9e12a456
ImZeusNC <lucasmedinaavila@protonmail.com>
2025-12-11 21:36:08 -0300

    add carrusel section

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+
+
 function activarMenu() {
     let menu = document.getElementById("menu-desplegable")
     menu.classList.toggle("menu-activo")
 }
+
+    var swiper = new Swiper(".swiper", {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      autoplay: {
+        delay: 2000,
+      },
+    });
